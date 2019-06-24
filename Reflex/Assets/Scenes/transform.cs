5eb2792450332356ed4225db4f3c13d993869d9d
Canberk İLDOĞAN <canberk.ildogan@nano.com.tr>
2019-06-24 16:20:00 +0300

    yeni versiyon

---
+++ Reflex/Assets/Scenes/transform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButton()
+    {
+        Application.LoadLevel("Game");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+        //UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
---
+++ Reflex/Assets/character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class character : MonoBehaviour
+{
+    private float deltaX, deltaY;
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            
+            switch(touch.phase)
+            {
+                case TouchPhase.Began:
+                    deltaX = touchPos.x - transform.position.x;
+                    break;
+                case TouchPhase.Moved:
+                    rb.MovePosition(new Vector2(touchPos.x - deltaX, 0));
+                    break;
+                case TouchPhase.Ended:
+                    rb.velocity = Vector2.zero;
+                    break;
+            }
+        }
+    }
+}
